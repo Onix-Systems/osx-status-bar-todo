4fa5337bdf02b26b7cfb1e702950c6715565eb59
derpoliuk <derpoliuk@gmail.com>
2016-12-21 18:15:04 +0200

    Remove button image and use "TODO" as a title for status bar button

--- todo-status-bar/AppDelegate.swift
+++ todo-status-bar/AppDelegate.swift
@@ -18,23 +18,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-    let statusItem = NSStatusBar.system().statusItem(withLength: -2)
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
         guard let button = statusItem.button else { return }
-        button.image = #imageLiteral(resourceName: "StatusBarButtonImage")
-//        button.action = #selector(AppDelegate.some(_:))
-
-        /*
-         let menu = NSMenu()
-
-         menu.addItem(NSMenuItem(title: "Print Quote", action: Selector("printQuote:"), keyEquivalent: "P"))
-         menu.addItem(NSMenuItem.separatorItem())
-         menu.addItem(NSMenuItem(title: "Quit Quotes", action: Selector("terminate:"), keyEquivalent: "q"))
-         
-         statusItem.menu = menu
- */
+        button.title = "TODO"
         let menu = NSMenu()
         let item1 = NSMenuItem(title: "1 title", action: #selector(AppDelegate.some(_:)), keyEquivalent: "P")
         item1.state = NSOnState
