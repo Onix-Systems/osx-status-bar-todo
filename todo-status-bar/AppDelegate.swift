b738a8c63a24906cacdda6d5669a78b910b0af9d
derpoliuk <derpoliuk@gmail.com>
2016-12-21 17:31:51 +0200

    Initial Commit

---
+++ todo-status-bar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  todo-status-bar
+//
+//  Created by derp on 12/21/16.
+//  Copyright © 2016 Onix-Systems. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
