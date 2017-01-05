530077bf6a436b32188d1246aa7b22136033a9e8
derpoliuk <derpoliuk@gmail.com>
2017-01-05 18:49:53 +0200

    Remove boilerplate code from AppDelegate

--- todo-status-bar/AppDelegate.swift
+++ todo-status-bar/AppDelegate.swift
@@ -21,10 +21,6 @@ class AppDelegate: NSObject, NSApplicationDelegate, EditTodosWindowControllerDel
         updateStatusItem()
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
     // MARK: - Validate Menu Item
 
     override func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
